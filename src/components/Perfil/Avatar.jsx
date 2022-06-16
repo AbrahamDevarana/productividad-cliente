de8ea8bb9b81a57908c75d1f5d19de1f472fcda2
AbrahamAG <abrahamalvarado@devarana.mx>
2022-06-16 15:33:49 -0500

    Usuarios ajustes2

--- src/components/Perfil/Avatar.jsx
+++ src/components/Perfil/Avatar.jsx
@@ -2,7 +2,7 @@ const Avatar = ({picture, userName, ...props}) => {
     return ( 
         <>
             {picture ? 
-                <img key={props.key} src={picture} alt={userName} className={`w-20 rounded-full shadow ${props.className ?? props.className}`} />
+                <img key={props.key} src={picture} alt={userName} className={`rounded-full shadow ${props.className ?? props.className}`} />
             :
                 <div key={props.key} className={`flex bg-gradient-to-tr from-gray-600 to-gray-400 rounded-full -mx-1 ${props.className ?? props.className}`}>
                     <span className="text-white m-auto">{props.children}</span>
